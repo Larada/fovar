687a2459a882458ba13c8bdb4efcbf0afee35e3c
larada <1244503897@qq.com>
2020-10-27 15:59:36 +0800

    91.c

---
+++ 91.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int a, b, hh;
+    printf("open a");
+    scanf("%d", &a);
+    printf("open b");
+    scanf("%d", &b);
+    hh = a * b;
+    
+    printf("%d", hh);
+	
+	return 0;
+    
+}
